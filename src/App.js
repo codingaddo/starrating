5deb3f2b0cb2577b6eb7daf86cf71b58d115fcbe
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-08-27 14:56:08 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import StarRating from './component/StarRating';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <StarRating defaultRating={0} maxRating={10} size={40}/>
+      <StarRating defaultRating={3} maxRating={5}/>
     </div>
   );
 }
