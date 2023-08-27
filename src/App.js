02fb0e10b1b81a84919e9cb1b684ae6321dac96b
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-08-27 15:48:30 +0000

    .

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import StarRating from './component/StarRating';
 function App() {
   return (
     <div >
-      <StarRating defaultRating={0} maxRating={10} size={40}/>
-      <StarRating defaultRating={3} maxRating={5}/>
+      <StarRating  maxRating={10} size={60} color='yellow'/>
+      <StarRating defaultRating={1} maxRating={8} color='green' size={52}/>
+      <StarRating defaultRating={2} maxRating={8} color='blue' size={40}/>
+      <StarRating defaultRating={4} maxRating={8} color='violet' size={35}/>
+      <StarRating defaultRating={3}  color='#ff1233' messages={['Vey Bad','Bad','Good','Better','Best']}/>
     </div>
   );
 }
--- src/component/StarRating.jsx
+++ src/component/StarRating.jsx
@@ -2,61 +2,75 @@ import React, { useState } from 'react'
 
 const StarRating = (
     {fill,
-        maxRating,
-        defaultRating=0
-        ,onSetRating,
+        maxRating=5,
+        defaultRating,
         size=24
         ,color,
-        message=[]
+        messages=[],
+        className
 
     }) => {
     const [rating,setRating] = useState(defaultRating)
+    const [temporalRating, setTemporalRating] = useState(0)
 
     const containerStyle ={
         display: "flex",
         gap:'50px',
         alignItems:'center',
-        fontSize:`${size}px`
+        fontSize:`${size}px`,
+        color:`${color}`
     }
 
     const starContainer ={
         display:'flex',
-        gap:'20px'
+        gap:'20px',
+        alignItems:'center',
+        cursor:'pointer'
     }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
         <div style={starContainer}>
             {Array.from({length:maxRating},(_,i)=>(
                     <Star
                     key={i}
                     onSetRating={()=>setRating(i+1)}
-                    fill={rating>=i+1}
+                    onHoverIn={()=>setTemporalRating(i+1)}
+                    onHoverOut={()=>setTemporalRating(0)}
+                    fill={temporalRating? temporalRating >=i+1 : rating >=i+1}
                     size={size}
                     color={color}
                     />
                 
             ))}
         </div>
-        <span>{rating}</span>
+        <span>{messages.length === maxRating? messages[temporalRating? temporalRating-1 : rating-1] : temporalRating || rating || ''}</span>
     </div>
   )
 }
 
 export default StarRating
 
-const Star = ({fill,onSetRating,size,color})=>{
+const Star = ({fill,onSetRating,size,color,onHoverIn,onHoverOut})=>{
     return(
         <div>
             {fill?
-            <span onClick={onSetRating}>
-                  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width={size} fill='' height={size} ><path d="M19.467,23.316,12,17.828,4.533,23.316,7.4,14.453-.063,9H9.151L12,.122,14.849,9h9.213L16.6,14.453Z"/></svg>
+            <span 
+            role='button'
+            onClick={onSetRating}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+            >
+                  <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width={size} fill={color} height={size} ><path d="M19.467,23.316,12,17.828,4.533,23.316,7.4,14.453-.063,9H9.151L12,.122,14.849,9h9.213L16.6,14.453Z"/></svg>
             </span>
             :
             <span
+            role='button'
             onClick={onSetRating}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill=''><g id="_01_align_center" data-name="01 align center"><path d="M19.467,23.316,12,17.828,4.533,23.316,7.4,14.453-.063,9H9.151L12,.122,14.849,9h9.213L16.6,14.453ZM12,15.346l3.658,2.689-1.4-4.344L17.937,11H13.39L12,6.669,10.61,11H6.062l3.683,2.691-1.4,4.344Z"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color} ><g id="_01_align_center" data-name="01 align center"><path d="M19.467,23.316,12,17.828,4.533,23.316,7.4,14.453-.063,9H9.151L12,.122,14.849,9h9.213L16.6,14.453ZM12,15.346l3.658,2.689-1.4-4.344L17.937,11H13.39L12,6.669,10.61,11H6.062l3.683,2.691-1.4,4.344Z"/></g></svg>
             </span>
             }
         </div>
