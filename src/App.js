d0b6a30366f49375b69a47e6e7842e0d27f7a248
Addo Michael <119521479+codingaddo@users.noreply.github.com>
2023-08-31 23:20:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StarRating from './component/StarRating';
 function App() {
   return (
     <div >
-      <StarRating  maxRating={10} size={60} color='yellow'/>
+      <StarRating  maxRating={10} size={60} color=''/>
       <StarRating defaultRating={1} maxRating={8} color='green' size={52}/>
       <StarRating defaultRating={2} maxRating={8} color='blue' size={40}/>
       <StarRating defaultRating={4} maxRating={8} color='violet' size={35}/>
